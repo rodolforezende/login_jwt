a0fd02f9552344ecb943964d13b514e761c9c28d
rodolforezende <rodolforrcont@gmail.com>
2021-11-13 07:50:01 -0300

    Study - Login JWT

---
+++ controllers/authController.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+const userController = require('./userController');
+
+module.exports = function (req, res, next) {
+  const token = req.header('authorization-token');
+  if (!token) return res.status(401).send('Acess Denied');
+
+  try {
+    const userVerified = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.user = userVerified;
+    next();
+  } catch (error) {
+    res.status(401).send('Acess Denied');
+  }
+}
---
+++ controllers/userController.js
@@ -0,0 +1,49 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { loginValidate, registerValidate } = require('../controllers/validate');
+
+const userController = {
+  register: async function register(req, res) {
+
+    const { error }= registerValidate(req.body);
+    if (error) return res.status(400).send(error.message);
+
+    const selectedUser = await User.findOne({ email: req.body.email });
+    if (selectedUser) return res.status(400).send('Email already exists');
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password)
+    })
+
+    try {
+      const savedUser = await user.save()
+      res.send(savedUser)
+    } catch (error) {
+      res.status(400).send(error)
+    }
+  },
+  login: async function login(req, res) {
+
+    const { error }= loginValidate(req.body);
+    if (error) return res.status(400).send(error.message);
+
+    const selectedUser = await User.findOne({ email: req.body.email });
+    if (!selectedUser) return res.status(400).send('Email or Password incorrect');
+
+    const passwordAndUserMatch = bcrypt.compareSync(req.body.password, selectedUser.password);
+    if (!passwordAndUserMatch) return res.status(400).send('Email or Password incorrect');
+
+    const token = jwt.sign({ _id: selectedUser._id, admin: selectedUser.admin }, process.env.TOKEN_SECRET);
+
+    res.header('authorization-token', token);
+    res.send('User Logged');
+  },
+}
+
+
+
+
+
+module.exports = userController;
